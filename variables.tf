d9be8c81ec3b5815f45305fbc30808aa81444297
Nikitha Kambhampati <kambhampati.ni@northeastern.edu>
2024-10-09 18:12:18 -0400

    testing workflow

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
   default     = "us-east-1" 
-
+}
 
 variable "vpc_count" {
   description = "Number of VPCs to create"
