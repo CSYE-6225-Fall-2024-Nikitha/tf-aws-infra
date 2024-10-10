8e50690ed7af61ef3e60f4f1fa3c86fd3e63bf81
Nikitha Kambhampati <kambhampati.ni@northeastern.edu>
2024-10-09 22:44:23 -0400

    changes to infra

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -40,3 +40,42 @@ resource "aws_internet_gateway" "main" {
     Name = "${var.name}-igw"
   }
 }
+
+# Create a public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.name}-public-rt"
+  }
+}
+
+# Associate public subnets with the public route table
+resource "aws_route_table_association" "public_subnets" {
+  count          = var.public_subnet_count
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Create a public route to the Internet Gateway
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+# Create a private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.name}-private-rt"
+  }
+}
+
+# Associate private subnets with the private route table
+resource "aws_route_table_association" "private_subnets" {
+  count          = var.private_subnet_count
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private.id
+}
