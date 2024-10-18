94d53dcaee14257c96db71ac77e4006bb60068c8
Nikitha Kambhampati <kambhampati.ni@northeastern.edu>
2024-10-18 19:28:40 -0400

    database security group

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -172,3 +172,30 @@ resource "aws_instance" "app_instance" {
     Name = "${var.name}-app-instance"
   }
 }
+
+
+resource "aws_security_group" "database_security_group" {
+  name        = "database_security_group"
+  description = "Security group for database security"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow TCP traffic on PostgreSQL port"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.application_security_group.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "database_security_group"
+  }
+}
+
