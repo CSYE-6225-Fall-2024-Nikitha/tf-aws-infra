27ec517f28fe49e2db8df38504796a04c8e908b4
Nikitha Kambhampati <kambhampati.ni@northeastern.edu>
2024-12-02 16:35:51 -0500

    remove ssh ingresS

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -108,15 +108,15 @@ resource "aws_security_group" "application_security_group" {
   vpc_id      = aws_vpc.main.id
 
   # Ingress rules (allow traffic on ports 22, 80, 443, and a custom app port)
-  ingress {
-    description      = "Allow SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+  # ingress {
+  #   description      = "Allow SSH"
+  #   from_port        = 22
+  #   to_port          = 22
+  #   protocol         = "tcp"
+  #   cidr_blocks      = ["0.0.0.0/0"]
+  #   ipv6_cidr_blocks = ["::/0"]
 
-  }
+  # }
 
   # ingress {
   #   description = "Allow HTTP"
